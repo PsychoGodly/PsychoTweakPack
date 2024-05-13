016129d7160088ace60c9af712fc5d74f2d25ac4
PsychoGodly <90983397+PsychoGodly@users.noreply.github.com>
2024-05-13 13:06:28 +0100

    Initial commit

---
+++ 3.Configs/4 Power Plan/2 Delete Other Power Plans.cmd
@@ -0,0 +1,6 @@
+@echo off
+echo Delete Balanced - Power Saving - High Performance
+powercfg -delete 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+powercfg -delete 381b4222-f694-41f0-9685-ff5bb260df2e
+powercfg -delete a1841308-3541-4fab-bc81-f71556f20b4a
+pause
---
+++ 3.Configs/6 Unwanted Services/7 Disable Mitigations.cmd
@@ -0,0 +1,3 @@
+@echo off
+PowerShell "ForEach($v in (Get-Command -Name \"Set-ProcessMitigation\").Parameters[\"Disable\"].Attributes.ValidValues){Set-ProcessMitigation -System -Disable $v.ToString() -ErrorAction SilentlyContinue}"
+pause
