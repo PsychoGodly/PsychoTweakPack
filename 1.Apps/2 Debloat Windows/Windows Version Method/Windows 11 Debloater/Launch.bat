708e41de79430d4c33c0b4396041cdc0088cfd0d
Fucking Psycho <90983397+PsychoGodly@users.noreply.github.com>
2025-07-17 00:35:34 +0100

    New Debloat Method by Windows version, and supporting 10 and 11

---
+++ 1.Apps/2 Debloat Windows/Windows Version Method/Windows 11 Debloater/Launch.bat
@@ -0,0 +1,32 @@
+@ECHO OFF
+SET CUR_DIR=%~dp0
+TITLE Windows 11 Debloater
+GOTO CHECK_MAIN_FILE
+
+:CHECK_MAIN_FILE
+	IF NOT EXIST "%CUR_DIR%\src\main.ps1" (
+		COLOR C
+		CLS
+		ECHO PowerShell files was not found, please make sure its there and run this script again.
+		ECHO Excepted path : "%CUR_DIR%src\main.ps1"
+		ECHO Press any key to close this script
+		PAUSE > NUL
+		EXIT
+	)
+	GOTO CHECK_ADMIN_PERMS
+
+:CHECK_ADMIN_PERMS
+	REG QUERY "HKEY_USERS\S-1-5-20" || (
+		CLS
+		COLOR C
+		CLS
+		ECHO Please run this script as Administrator
+		PAUSE > NUL
+		EXIT
+	)
+	CLS
+	GOTO START_SCRIPT
+
+:START_SCRIPT
+	powershell -NoProfile -ExecutionPolicy Bypass -File "%CUR_DIR%\src\main.ps1"
+	EXIT
---
+++ 1.Apps/2 Debloat Windows/Windows Version Method/Windows 11 Debloater/README.md
@@ -0,0 +1,9 @@
+# Windows 11 Debloater 🚀
+
+This script will be re-designed. 
+
+⚠️ DISCLAIMER: You're using this at your own risk, I'm not responsible for any damage or data loss.
+
+# How to use?
+
+To start this script, run ``Launch.bat`` as Administrator!
